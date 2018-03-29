840c65a603c4684ff9565edd4f105405ce900293
Anja <anja.andric@udg.edu.me>
2018-03-29 10:30:14 +0200

    Task2

--- task2.py
+++ task2.py
@@ -13,3 +13,37 @@
 """
 
 # Write your code here
+
+
+def product_digits(number):
+
+    # Provjeravamo da li je intiger
+    if not isinstance(number, int):
+        return False
+
+    # Initialize product value
+    product_sum = 1
+
+
+    # NOTE: Napravimo je da radi za negativne brojeve
+
+    number = abs(number)
+
+
+    while number > 0:
+        digit = number % 10
+        number = number // 10
+        product_sum *= digit
+
+    return product_sum
+
+
+def main():
+
+    int_number = 1234
+    product_sum = product_digits(int_number)
+    print("Proizvod cifara je : ", product_sum)
+
+main()
+
+
